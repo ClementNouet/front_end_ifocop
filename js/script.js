7e97a8d6233a33b4dee583d9faa37a26e687a9ad
Clément NOUET <nouet.clem@gmail.com>
2024-07-23 12:39:39 +0200

    Mise à jour V2 ajout page produit/evenement/contact

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// script.js
+// slide 1
 document.addEventListener('DOMContentLoaded', () => {
     const slider = document.querySelector('.slider');
     const slides = document.querySelectorAll('.slide');
@@ -12,6 +12,31 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(showNextSlide, 4000);
 });
 
+// slide2
+document.addEventListener('DOMContentLoaded', () => {
+    const slider = document.querySelector('.slider2');
+    const slides = document.querySelectorAll('.slide2');
+    let currentIndex = 0;
+
+    function showNextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        slider.style.transform = `translateX(-${currentIndex * 100}vw)`;
+    }
+
+    setInterval(showNextSlide, 4000);
+});
+
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 100) {
+        nav.classList.add('scrolled');
+        nav.classList.remove('none');
+    } else {
+        nav.classList.remove('scrolled');
+        nav.classList.add('none');
+    }
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     var containers = document.querySelectorAll('.text-container');
     containers.forEach(function(container) {
@@ -23,6 +48,35 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 button.textContent = 'Voir plus';
             }
+        });
+    });
+});
+
+//Filtrage des produits
+document.addEventListener('DOMContentLoaded', function() {
+    const filterButtons = document.querySelectorAll('.filter-btn');
+    const columns = document.querySelectorAll('.column-content');
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Enlever la classe 'active' de tous les boutons
+            filterButtons.forEach(btn => btn.classList.remove('active'));
+            
+            // Ajouter la classe 'active' au bouton cliqué
+            button.classList.add('active');
+
+            const filter = button.getAttribute('data-filter');
+            columns.forEach(column => {
+                if (filter === 'all') {
+                    column.style.display = 'block';
+                } else {
+                    if (column.getAttribute('data-category') === filter) {
+                        column.style.display = 'block';
+                    } else {
+                        column.style.display = 'none';
+                    }
+                }
+            });
         });
     });
 });
